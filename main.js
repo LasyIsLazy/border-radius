ac8ea178a42194ba49c9d8245cdae9e9368cd8d1
Liushuyuan <liushuyuan_i@didichuxing.com>
2019-04-17 12:14:37 +0800

    生成代码功能

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-function changeRadius(ele, radius, unit) {
-  unit || (unit = '%')
-  const xRadius = radius.filter(function (e, idx) { return !(idx % 2) })
-  const yRadius = radius.filter(function (e, idx) { return idx % 2 })
-  const radiusStr = xRadius.map(function (e) { return e + unit }).join(' ') + ' /' + yRadius.map(function (e) { return e + unit }).join(' ')
-  ele.style.borderRadius = radiusStr
-}
 window.onload = function () {
+  function changeRadius(ele, radius, unit) {
+    unit || (unit = '%')
+    const xRadius = radius.filter(function (e, idx) { return !(idx % 2) })
+    const yRadius = radius.filter(function (e, idx) { return idx % 2 })
+    const radiusStr = xRadius.map(function (e) { return e + unit }).join(' ') + ' / ' + yRadius.map(function (e) { return e + unit }).join(' ')
+    ele.style.borderRadius = radiusStr
+    codePre.innerText = 'border-radius: ' + ele.style.borderRadius + ';'
+  }
   let unit = '%'
   const rect = document.getElementById('rect')
   const tl1 = document.getElementById('tl1')
@@ -19,6 +20,7 @@ window.onload = function () {
   const unitSelect = document.getElementById('unit')
   const recWidthInp = document.getElementById('recWidth')
   const recHeightInp = document.getElementById('recHeight')
+  const codePre = document.getElementById('code')
   const eles = [tl1, tl2, tr1, tr2, br1, br1, bl1, bl2]
   const radius = eles.map(function (ele) { return 0 })
   eles.forEach(function (ele, idx) {
