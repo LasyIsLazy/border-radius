62283e5a76c0519c6a9e6bbcb8a1fc58d598dd2c
Liushuyuan <liushuyuan_i@didichuxing.com>
2019-04-17 10:48:36 +0800

    完成修改矩形宽度高度的功能

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function changeRadius(ele, radius, unit) {
   ele.style.borderRadius = radiusStr
 }
 window.onload = function () {
+  let unit = '%'
   const rect = document.getElementById('rect')
   const tl1 = document.getElementById('tl1')
   const tl2 = document.getElementById('tl2')
@@ -15,13 +16,26 @@ window.onload = function () {
   const br2 = document.getElementById('br2')
   const bl1 = document.getElementById('bl1')
   const bl2 = document.getElementById('bl2')
+  const unitSelect = document.getElementById('unit')
+  const recWidthInp = document.getElementById('recWidth')
+  const recHeightInp = document.getElementById('recHeight')
   const eles = [tl1, tl2, tr1, tr2, br1, br1, bl1, bl2]
   const radius = eles.map(function (ele) { return 0 })
   eles.forEach(function (ele, idx) {
     ele.addEventListener('input', function (e) {
       const val = parseInt(e.target.value)
       radius[idx] = val
-      changeRadius(rect, radius)
+      changeRadius(rect, radius, unit)
     })
+  })
+  unitSelect.addEventListener('change', function () {
+    unit = unitSelect.value
+    changeRadius(rect, radius, unit)
+  })
+  recWidthInp.addEventListener('input', function () {
+    rect.style.width = recWidthInp.value + 'px'
+  })
+  recHeightInp.addEventListener('input', function () {
+    rect.style.height = recHeightInp.value + 'px'
   })
 }
